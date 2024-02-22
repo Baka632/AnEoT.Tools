55ea0d4fb72156295bdb50d8ba8762f0755d8a5f
Baka632 <stevemc123456@outlook.com>
2024-02-23 00:19:22 +0800

    增强错误处理

--- src/AnEoT.Tools.WordToMarkdown/ViewModels/MainViewModel.cs
+++ src/AnEoT.Tools.WordToMarkdown/ViewModels/MainViewModel.cs
@@ -30,9 +30,24 @@ public sealed partial class MainViewModel : ObservableObject
 
         if (isOk == true)
         {
-            using WordprocessingDocument doc = WordprocessingDocument.Open(dialog.FileName, false);
-            string markdown = WordToMarkdownService.GetMarkdown(doc);
-            MarkdownString = markdown;
+            try
+            {
+                using WordprocessingDocument doc = WordprocessingDocument.Open(dialog.FileName, false);
+                string markdown = WordToMarkdownService.GetMarkdown(doc);
+                MarkdownString = markdown;
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("文件正在被其他进程使用。", "警告", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (FileFormatException)
+            {
+                MessageBox.Show("此文件似乎不是有效的 docx 文件。", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"出现未知错误。\n{ex}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         IsLoading = false;
