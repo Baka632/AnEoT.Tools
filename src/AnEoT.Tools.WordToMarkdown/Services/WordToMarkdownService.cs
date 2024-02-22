996d0d1974f3c3af46cde90781eb3e95a88dac4c
Baka632 <stevemc123456@outlook.com>
2024-02-22 21:16:38 +0800

    添加更多样式支持

--- src/AnEoT.Tools.WordToMarkdown/Services/WordToMarkdownService.cs
+++ src/AnEoT.Tools.WordToMarkdown/Services/WordToMarkdownService.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿// TODO: 这里的代码一团糟，以后一定要重构
+
+using System.Text;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
@@ -21,28 +23,148 @@ public static class WordToMarkdownService
                 {
                     ParagraphProperties? paraProp = paragraph.ParagraphProperties;
 
+                    #region 读取段落样式
+                    Style? paraStyle = null;
+                    if (string.IsNullOrWhiteSpace(paraProp?.ParagraphStyleId?.Val?.Value) != true && mainDocumentPart?.StyleDefinitionsPart?.Styles is not null)
+                    {
+                        paraStyle = (Style?)mainDocumentPart.StyleDefinitionsPart.Styles.FirstOrDefault(element =>
+                        {
+                            return element is Style style && style.StyleId == paraProp.ParagraphStyleId.Val.Value;
+                        });
+                    }
+                    #endregion
+
+                    #region 写 Markdown 相关的东西
+
+                    #region Markdown 标题
                     if (paraProp?.OutlineLevel?.Val != null)
                     {
                         WriteMarkdownHeaderByOutlineLevel(stringBuilder, paraProp.OutlineLevel);
                     }
-                    else if (string.IsNullOrWhiteSpace(paraProp?.ParagraphStyleId?.Val?.Value) != true && mainDocumentPart?.StyleDefinitionsPart?.Styles is not null)
+                    else if (paraStyle is not null && paraStyle?.StyleParagraphProperties?.OutlineLevel is not null)
                     {
-                        Style? targetStyle = (Style?)mainDocumentPart.StyleDefinitionsPart.Styles.FirstOrDefault(element => element is Style style && style.StyleId == paraProp.ParagraphStyleId.Val.Value);
-                        if (targetStyle != null && targetStyle.StyleParagraphProperties?.OutlineLevel is not null)
+                        WriteMarkdownHeaderByOutlineLevel(stringBuilder, paraStyle.StyleParagraphProperties.OutlineLevel);
+                    }
+                    #endregion
+
+                    #region 加粗 + 倾斜 + 删除线 + 下划线
+                    bool isBold = false;
+                    bool isStrike = false;
+                    bool isItalic = false;
+                    bool isUnderline = false;
+                    if (paraProp?.ParagraphMarkRunProperties is not null)
+                    {
+                        DetermineRunPropertiesByChildElement(paraProp.ParagraphMarkRunProperties.ChildElements,
+                                                             ref isBold,
+                                                             ref isStrike,
+                                                             ref isItalic,
+                                                             ref isUnderline);
+                    }
+                    else if (paraStyle is not null && paraStyle?.StyleRunProperties is not null)
+                    {
+                        DetermineRunPropertiesByChildElement(paraStyle.StyleRunProperties.ChildElements,
+                                                             ref isBold,
+                                                             ref isStrike,
+                                                             ref isItalic,
+                                                             ref isUnderline);
+                    }
+                    #endregion
+
+                    #endregion
+
+                    #region 写文本
+
+                    if (isUnderline)
+                    {
+                        stringBuilder.Append("<u>");
+                    }
+
+                    if (isStrike)
+                    {
+                        stringBuilder.Append("~~");
+                    }
+
+                    if (isItalic)
+                    {
+                        stringBuilder.Append('*');
+                    }
+
+                    if (isBold)
+                    {
+                        stringBuilder.Append("**");
+                    }
+
+                    foreach (Run run in paragraph.Descendants<Run>())
+                    {
+                        if (run is not null)
                         {
-                            WriteMarkdownHeaderByOutlineLevel(stringBuilder, targetStyle.StyleParagraphProperties.OutlineLevel);
+                            string? colorHex = run.RunProperties?.Color?.Val?.Value;
+
+                            if (colorHex is not null)
+                            {
+                                stringBuilder.Append($"""<span style="color:#{colorHex}">""");
+                            }
+
+                            stringBuilder.Append(run.InnerText);
+
+                            if (colorHex is not null)
+                            {
+                                stringBuilder.Append("</span>");
+                            }
                         }
                     }
 
-                    stringBuilder.Append(paragraph.InnerText);
-
-                    if (paraProp?.Justification?.Val != null)
+                    if (isBold)
                     {
-                        if (paraProp.Justification.Val.Value == JustificationValues.Right)
-                        {
-                            stringBuilder.Append("{.aright}");
-                        }
+                        stringBuilder.Append("**");
                     }
+
+                    if (isItalic)
+                    {
+                        stringBuilder.Append('*');
+                    }
+
+                    if (isStrike)
+                    {
+                        stringBuilder.Append("~~");
+                    }
+
+                    if (isUnderline)
+                    {
+                        stringBuilder.Append("</u>");
+                    }
+                    #endregion
+
+                    #region 写样式
+                    List<string> classList = new(3);
+
+                    #region 段落布局设置
+                    if (paraProp?.Justification?.Val is not null)
+                    {
+                        AddLayoutByJustificationValue(classList, paraProp.Justification.Val);
+                    }
+                    else if (paraStyle?.StyleParagraphProperties?.Justification?.Val is not null)
+                    {
+                        AddLayoutByJustificationValue(classList, paraStyle.StyleParagraphProperties.Justification.Val);
+                    }
+                    #endregion
+
+                    if (classList.Count != 0)
+                    {
+                        stringBuilder.Append('{');
+
+                        if (classList.Count == 1)
+                        {
+                            stringBuilder.Append(classList[0]);
+                        }
+                        else
+                        {
+                            stringBuilder.Append($" {string.Join(' ', classList)} ");
+                        }
+                        
+                        stringBuilder.Append('}');
+                    }
+                    #endregion
 
                     stringBuilder.AppendLine();
                 }
@@ -57,6 +179,32 @@ public static class WordToMarkdownService
         }
 
         return stringBuilder.ToString();
+    }
+
+    private static void DetermineRunPropertiesByChildElement(OpenXmlElementList childElements, ref bool isBold, ref bool isStrike, ref bool isItalic, ref bool isUnderline)
+    {
+        isBold = childElements.Any(element => element is Bold bold);
+        isStrike = childElements.Any(element => element is Strike strike);
+        isItalic = childElements.Any(element => element is Italic italic);
+        isUnderline = childElements.Any(element => element is Underline underline);
+    }
+
+    private static void AddLayoutByJustificationValue(List<string> classList, JustificationValues justification)
+    {
+        ArgumentNullException.ThrowIfNull(classList);
+
+        if (justification == JustificationValues.Right)
+        {
+            classList.Add(".aright");
+        }
+        else if (justification == JustificationValues.Left)
+        {
+            classList.Add(".aleft");
+        }
+        else if (justification == JustificationValues.Center)
+        {
+            classList.Add(".centering");
+        }
     }
 
     private static void WriteMarkdownHeaderByOutlineLevel(StringBuilder writer, OutlineLevel outlineLevel)
