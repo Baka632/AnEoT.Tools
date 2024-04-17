bd38ef17864c171351e4b3e64a6ec6830462a577
Baka632 <stevemc123456@outlook.com>
2024-04-18 01:18:43 +0800

    初步添加检查 Markdown 文件的工具

---
+++ src/AnEoT.Tools.MarkdownChecker/CheckerBase.cs
@@ -0,0 +1,32 @@
+﻿using Markdig;
+using Microsoft.Extensions.Logging;
+
+namespace AnEoT.Tools.MarkdownChecker;
+
+internal partial class CheckerBase
+{
+    protected static readonly MarkdownPipeline MarkdownPipeline = new MarkdownPipelineBuilder()
+        .UseEmphasisExtras(Markdig.Extensions.EmphasisExtras.EmphasisExtraOptions.Default)
+        .UseAdvancedExtensions()
+        .UseListExtras()
+        .UseYamlFrontMatter()
+        .Build();
+    protected static readonly ILogger Logger;
+    protected static readonly HttpClient HttpClient = new()
+    {
+        Timeout = TimeSpan.FromSeconds(20),
+    };
+
+    static CheckerBase()
+    {
+        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+        ILogger logger = factory.CreateLogger("Markdown Analyzer");
+        Logger = logger;
+    }
+
+    [LoggerMessage(EventId = 2, Level = LogLevel.Error, Message = "{FilePath}({TargetLine}): 无法访问链接：{Link}")]
+    public static partial void LogCannotAccessLink(ILogger logger, string filePath, int targetLine, string link);
+    
+    [LoggerMessage(EventId = 3, Level = LogLevel.Error, Message = "{FilePath}({TargetLine}): 找不到文件：{Link}。已尝试在以下路径中寻找：{TriedFilePath}")]
+    public static partial void LogCannotFindFile(ILogger logger, string filePath, int targetLine, string link, string triedFilePath);
+}
---
+++ src/AnEoT.Tools.MarkdownChecker/FolderChecker.cs
@@ -0,0 +1,29 @@
+﻿namespace AnEoT.Tools.MarkdownChecker;
+
+internal sealed class FolderChecker : CheckerBase
+{
+    public static async Task CheckDirectory(string path, string? rootPath, bool isRecursive)
+    {
+        DirectoryInfo directoryInfo = new(path);
+
+        rootPath ??= path;
+
+        await CheckDirectoryCore(directoryInfo, rootPath, isRecursive);
+    }
+
+    private static async Task CheckDirectoryCore(DirectoryInfo directoryInfo, string rootPath, bool isRecursive)
+    {
+        foreach (FileInfo file in directoryInfo.EnumerateFiles("*.md"))
+        {
+            await FileChecker.CheckSingleFile(file.FullName, rootPath);
+        }
+
+        if (isRecursive)
+        {
+            foreach (DirectoryInfo directory in directoryInfo.EnumerateDirectories())
+            {
+                await CheckDirectoryCore(directory, rootPath, isRecursive);
+            }
+        }
+    }
+}
